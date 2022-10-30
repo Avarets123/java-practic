12fdfb902e8750daf962d67d0a2ff182d18e0e97
Osman <avarets1234@gmail.com>
2022-10-30 15:15:31 +0300

    refactor streamIO

--- StreamIO/src/realworld/UserRepository.java
+++ StreamIO/src/realworld/UserRepository.java
@@ -7,20 +7,64 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserRepository implements EntityRepository<User>{
+public class UserRepository implements EntityRepository<User> {
 
-    private  final String fileName;
+    private final String fileName;
 
     public UserRepository(String fileName) {
         this.fileName = fileName;
     }
 
-    @Override
+//    @Override
+//    public List<User> findAll() {
+//        List<User> array = new ArrayList<>();
+//        Reader fileReader;
+//        BufferedReader reader;
+//        try {
+//            fileReader = new FileReader(fileName);
+//            reader = new BufferedReader(fileReader);
+//
+//            String userReadLine = reader.readLine();
+//            while (userReadLine != null) {
+//                String[] parts = userReadLine.split("\\|");
+//
+//                Long id = Long.parseLong(parts[0]);
+//                String firstName = parts[1];
+//                String lastName = parts[2];
+//                Integer age = Integer.parseInt(parts[3]);
+//                array.add(new User(id, firstName, lastName, age));
+//                userReadLine = reader.readLine();
+//            }
+//
+//        } catch (IOException e) {
+//            System.out.println("Что-то пошло не так !");
+//            throw new IllegalArgumentException(e);
+//        } finally {
+//            if (reader != null) {
+//                try {
+//                    reader.close();
+//
+//                } catch (IOException ignored){}
+//
+//            }
+//            if (fileReader != null) {
+//                try {
+//                    fileReader.close();
+//
+//                } catch (IOException ignored) {}
+//            }
+//
+//        }
+//        return array;
+//    }
+
+        @Override
     public List<User> findAll() {
         List<User> array = new ArrayList<>();
-        try {
-            Reader fileReader = new FileReader(fileName);
-            BufferedReader reader = new BufferedReader(fileReader);
+        try (
+                Reader fileReader = new FileReader(fileName);
+                BufferedReader reader = new BufferedReader(fileReader);
+                ){
 
             String userReadLine = reader.readLine();
             while (userReadLine != null) {
@@ -40,4 +84,5 @@ public class UserRepository implements EntityRepository<User>{
         }
         return array;
     }
+
 }
