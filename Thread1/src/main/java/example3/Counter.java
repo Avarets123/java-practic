39e407c3376604c56dbd46be2d399eb2d5d0c963
Osman <avarets1234@gmail.com>
2022-11-06 19:45:41 +0300

    add thred-synchronized

---
+++ Thread1/src/main/java/example3/Counter.java
@@ -0,0 +1,17 @@
+package example3;
+
+public class Counter {
+  private int value;
+
+  public int getValue() {
+    return value;
+  }
+
+  public synchronized void inc() {
+    value++;
+  }
+
+  public synchronized void dec() {
+    value--;
+  }
+}
