e698e1583264a5bce96d6e1facbcd603fc359545
Osman <avarets1234@gmail.com>
2022-11-06 12:37:15 +0300

    Thread

--- Thread1/src/main/java/example/Main.java
+++ Thread1/src/main/java/example/Main.java
@@ -8,15 +8,27 @@ public class Main {
       @Override
       public void run() {
         for (int i = 0; i < 50; i++) {
+          if (Thread.currentThread().isInterrupted()) {
+            break;
+          }
           System.out.println("Have a nice day");
+          try {
+            Thread.sleep(100);
+          } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+          }
         }
       }
     });
 
-
-
     thread.start();
     System.out.println("Start");
+    for (int i = 0; i < 50; i++) {
+      System.out.println("M");
+    }
     System.out.println("Finish");
+//    thread.stop();
+    thread.interrupt();
   }
+
 }
